cf046818b7bc0cffa506dfb7441daf418182e287
Dimitrii Lipiridi <dimitrii.lipiridi@delasport.com>
2024-08-24 09:53:52 +0300

    Remove nexus publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     signing
 
     id("com.diffplug.spotless") version "6.25.0"
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 group = "io.github.lipiridi"
@@ -75,15 +74,6 @@ publishing {
                     }
                 }
             }
-        }
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
     }
 }
